73e17b9a754952d91709a6880ab532e6c957b3c1
Ramzi BOUZAIENE <ramzi.bouzaiene@proxym-it.com>
2024-12-23 15:59:01 +0100

    adding sentry to trace the bugs and performance of the web site

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import * as Sentry from '@sentry/react'
+
+Sentry.init({
+  dsn: 'https://924d0aea02b7753589cadec1c318fed6@o4508517938233344.ingest.de.sentry.io/4508517940068432',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
